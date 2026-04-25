193365fbd7716619950710cb5c7ae1ea63d6d236
swagnik10 <swagnikghosh07@gmail.com>
2026-04-25 14:44:48 +0530

    Navbar development

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+import Navbar from './components/Navbar'
+
+const App = () => {
+
+  const [theme, setTheme] = useState(localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light')
+
+  
+
+  return (
+    <div className='dark:bg-black relative'>
+        <Navbar theme={theme} setTheme={setTheme} />
+    </div>
+  )
+}
+
+export default App
